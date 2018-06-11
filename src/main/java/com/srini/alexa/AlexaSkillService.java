e74a48212f6a12f186c235548585516c68c17001
Kunkalaguntla <617536@cognizant.com>
2018-06-11 16:24:14 -0400

    package change

--- src/main/java/com/srini/alexa/AlexaSkillService.java
+++ src/main/java/com/srini/alexa/AlexaSkillService.java
@@ -1,6 +1,12 @@
 package com.srini.alexa;
  
+import java.io.StringReader;
+import java.io.StringWriter;
+
 import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonWriter;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,15 +15,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
 import com.srini.alexa.model.request.Product;
 import com.srini.alexa.model.request.Status;
-import com.srini.alexa.model.response.AlexaResponse;
-import com.srini.alexa.model.response.Response;
-import com.srini.alexa.model.response.SessionAttributes;
  
 @Path("/alexaskill")
 public class AlexaSkillService {
@@ -42,65 +41,51 @@ public class AlexaSkillService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public String alexaIn(String alexaRequest) throws Exception {
-		parseJsonRequestString(alexaRequest);
-//		String output = "Alexa say : " + alexaRequest.getRequest().getType();
-//		SessionAttributes sessionAttributes = new SessionAttributes();
-//		sessionAttributes.setKey("1");
-//		Response response = new Response();
-//		OutputSpeech outputSpeech = new OutputSpeech();
-//		outputSpeech.setType("PlainText");
-//		outputSpeech.setText("Plain text string to speak");
-//		outputSpeech.setSsml("<speak>SSML text string to speak</speak>");
-//		response.setOutputSpeech(outputSpeech);
-//		Reprompt reprompt = new Reprompt();
-//		OutputSpeech RepromptOutputSpeech = new OutputSpeech();
-//		RepromptOutputSpeech.setType("PlainText");
-//		RepromptOutputSpeech.setText("Plain text string to speak");
-//		RepromptOutputSpeech.setSsml("<speak>SSML text string to speak</speak>");
-//		reprompt.setOutputSpeech(RepromptOutputSpeech);
-//		response.setReprompt(reprompt);
-//		String shouldEndSession = "false";
-//		response.setShouldEndSession(shouldEndSession);
-//		response.setCard(new Card());
-		return buildResponse();
+		
+		JsonReader reader = Json.createReader(new StringReader(alexaRequest));
+        
+        JsonObject alexaRequestObject = reader.readObject();
+        
+        JsonObject session_jsondata = alexaRequestObject.getJsonObject("session");
+        JsonObject context_jsondata = alexaRequestObject.getJsonObject("context");
+        JsonObject request_jsondata = alexaRequestObject.getJsonObject("request");
+		
+		JsonObject sessionJson = Json.createObjectBuilder()
+				.add("questionNo", "1")
+				.build();
+		
+		return buildResponse("SSML", "outputSpeechText", "repromptoutputSpeechText", "false", sessionJson);
+		
     }
 	
-	private void parseJsonRequestString(String jsonRequest) throws Exception {
-		System.out.println("Elements input json"+jsonRequest);
-		//Parse the JSON data present in the string format
-		JSONParser parse = new JSONParser();
-		//Type caste the parsed json data in json object
-		JSONObject jobj = (JSONObject)parse.parse(jsonRequest);
-		System.out.println("Elements input json jobj"+jobj);
-		//Store the JSON object in JSON array as objects (For level 1 array element i.e Results)
-		JSONArray jsonarr_1 = (JSONArray) jobj.get("session");
-		System.out.println("Elements input json jsonarr_1"+jsonarr_1);
-		//Get data for Results array
-		for(int i=0;i<jsonarr_1.size();i++)
-		{
-		  //Store the JSON objects in an array
-		  //Get the index of the JSON object and print the values as per the index
-		  JSONObject jsonobj_1 = (JSONObject)jsonarr_1.get(i);
-		  //Store the JSON object in JSON array as objects (For level 2 array element i.e Address Components)
-		  JSONArray jsonarr_2 = (JSONArray) jsonobj_1.get("application");
-		  System.out.println("Elements under results array");
-		  System.out.println("\nSession id: " +jsonobj_1.get("sessionId"));
-		  System.out.println("new: " +jsonobj_1.get("new"));
-		  //Get data for the Address Components array
-		  System.out.println("Elements under address_components array");
-		  System.out.println("The long names, short names and types are:");
-		  for(int j=0;j<jsonarr_2.size();j++)
-		  {
-		     //Same just store the JSON objects in an array
-		     //Get the index of the JSON objects and print the values as per the index
-		     JSONObject jsonobj_2 = (JSONObject) jsonarr_2.get(j);
-		     //Store the data as String objects
-		     String str_data1 = (String) jsonobj_2.get("applicationId");
-		     System.out.println(str_data1);
-		    
-		      System.out.println("\n");
-		  }
-		}
+private String buildResponse(String outputSpeechType, String speech, String repromptspeech, String shouldEndSession, JsonObject sessionJson) {
+		
+		JsonObject alexaJsonResponseObj = Json.createObjectBuilder()
+                .add("version", "1.0")
+                .add("sessionAttributes", sessionJson)
+                .add("response", Json.createObjectBuilder()
+                					.add("shouldEndSession", shouldEndSession)
+                					.add("outputSpeech", Json.createObjectBuilder()
+                											.add("type", outputSpeechType)
+                											.add("ssml", speech)
+                											.build()
+                						)
+                					.add("reprompt", Json.createObjectBuilder()
+											.add("outputSpeech", Json.createObjectBuilder()
+        											.add("type", outputSpeechType)
+        											.add("ssml", speech)
+        											.build()
+        										)
+                						)
+                	)
+                .build();
+         
+        StringWriter stringWriter = new StringWriter();
+        JsonWriter writer = Json.createWriter(stringWriter);
+        writer.writeObject(alexaJsonResponseObj);
+        writer.close();
+        return stringWriter.getBuffer().toString();
+		
 	}
 	
 	@POST
@@ -112,22 +97,22 @@ public class AlexaSkillService {
         return new Status("SUCCESS", "Inserted " + product.getName());
     }
 	
-	private AlexaResponse buildResponse(SessionAttributes sessionAttributes, Response response) {
-		AlexaResponse alexaResponse = new AlexaResponse();
-		alexaResponse.setVersion("1.0");
-		alexaResponse.setSessionAttributes(sessionAttributes);
-		alexaResponse.setResponse(response);
-		return alexaResponse;
-	}
-	
-	private String buildResponse() {
-		String json = Json.createObjectBuilder()
-	            .add("key1", "value1")
-	            .add("key2", "value2")
-	            .build()
-	            .toString();
-		return json;
-	}
+//	private AlexaResponse buildResponse(SessionAttributes sessionAttributes, Response response) {
+//		AlexaResponse alexaResponse = new AlexaResponse();
+//		alexaResponse.setVersion("1.0");
+//		alexaResponse.setSessionAttributes(sessionAttributes);
+//		alexaResponse.setResponse(response);
+//		return alexaResponse;
+//	}
+//	
+//	private String buildResponse() {
+//		String json = Json.createObjectBuilder()
+//	            .add("key1", "value1")
+//	            .add("key2", "value2")
+//	            .build()
+//	            .toString();
+//		return json;
+//	}
 	
 	String json = Json.createObjectBuilder()
             .add("key1", "value1")
